048666596e99ea259bc7f2e73b06ac6ddaf5d373
valeriyaduran <valeriya.duran@gmail.com>
2024-01-25 16:10:47 +0300

    migration added target metadata added

--- migrations/env.py
+++ migrations/env.py
@@ -9,6 +9,8 @@ from alembic import context
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
+from src.models.link import Base
+
 config = context.config
 
 # Interpret the config file for Python logging.
@@ -20,7 +22,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
